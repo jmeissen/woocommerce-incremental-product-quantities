7b1a465c2ef1bd8eb51057cf9b5a28c9c059af1b
Jeffrey Meissen <jeffrey@digitpark.nl>
2022-11-23 01:33:22 +0100

    Fix: $role index of user roles
    Rewritten a less verbose version of the function

--- includes/ipq-functions.php
+++ includes/ipq-functions.php
@@ -28,110 +28,74 @@ function wpbo_get_applied_rule( $product, $role = null ) {
 }
 
 /**
- *	Get the Rule Object thats being applied to a given product.
- *	Will return null if no rule is applied.
+ *  Get the Rule Object thats being applied to a given product.
+ *  Will return null if no rule is applied.
  *
- *	@params object	$product WC_Product object
- *	@param 	string	User role to get rule from, otherwise current user role is used
- *	@return mixed 	Null if no rule applies / Object top rule post
+ *  @param  object $product WC_Product object.
+ *  @param  string $role  User role to get rule from, otherwise current user role is used.
+ *  @return mixed  Null if no rule applies / Object top rule post
  */
 function wpbo_get_applied_rule_obj( $product, $role = null ) {
 
-	// Get Product Terms
-	$product_cats = wp_get_post_terms( $product->get_id(), 'product_cat' );
-	$product_tags = wp_get_post_terms( $product->get_id(), 'product_tag' );
-
-	// Get role if not passed
-	if(!is_user_logged_in()) {
+	// Get role if not passed.
+	if ( ! is_user_logged_in() || ( null === $role && null === ( $role = array_pop( get_userdata( get_current_user_id() )->roles ) ) ) ) {
 		$role = 'guest';
-	} else if ( $role == NULL ) {
-		$user_data = get_userdata( get_current_user_id() );
-		if ( $user_data->roles ) {
-			foreach ( $user_data->roles as $cap => $val ) {
-				$role = $cap;
-			}
-		}
 	}
 
-	// Combine all product terms
-	$product_terms = array_merge( $product_cats, $product_tags );
-
-	// Check for rule / role transient
+	// Check for rule / role transient.
 	if ( false === ( $rules = get_transient( 'ipq_rules_' . $role ) ) ) {
 
-		// Get all Rules
-		$args = array(
-			'posts_per_page'   	=> -1,
-			'post_type'        	=> 'quantity-rule',
-			'post_status'      	=> 'publish',
+		// Filter applicable rules by role.
+		$rules = array_filter(
+			get_posts(
+				array(
+					'posts_per_page' => -1,
+					'post_type'      => 'quantity-rule',
+					'post_status'    => 'publish',
+				)
+			),
+			function( $rule ) use ( $role ) {
+				return in_array( $role, get_post_meta( $rule->ID, '_roles' )[0], true );
+			}
 		);
 
-		$rules = get_posts( $args );
-
-		// Remove rules not applied to current user role
-		$cnt = 0;
-		$rules_to_unset = array();
-
-		while ( $cnt < count( $rules ) ) {
-
-			$roles = get_post_meta( $rules[$cnt]->ID, '_roles' );
-		 	if ( !in_array( $role, $roles[0] ) && !empty($roles[0])) {
-			 	array_push( $rules_to_unset, $cnt );
-		 	}
-
-		 	$cnt++;
-		}
-
 		$duration = 60 * 60 * 12; // 12 hours
-		arsort( $rules_to_unset );
-
-		foreach ( $rules_to_unset as $single_unset ) {
-			unset( $rules[$single_unset] );
-		}
 		set_transient( 'ipq_rules_' . $role, $rules, $duration );
 	}
 
-	$top = null;
+	$top      = null;
 	$top_rule = null;
 
-	// Loop through the rules and find the ones that apply
+	// Loop through the rules and find the ones that apply.
 	foreach ( $rules as $rule ) {
 
-	 	$apply_rule = false;
+		// Get the Rule's Cats and Tags.
+		$cats = get_post_meta( $rule->ID, '_cats' );
+		if ( $cats ) {
+			$cats = $cats[0];
+		}
 
-	 	// Get the Rule's Cats and Tags
-	 	$cats = get_post_meta( $rule->ID, '_cats' );
-	 	$tags = get_post_meta( $rule->ID, '_tags' );
-	 	$roles = get_post_meta( $rule->ID, '_roles' );
+		$tags = get_post_meta( $rule->ID, '_tags' );
+		if ( $tags ) {
+			$tags = $tags[0];
+		}
 
-	 	if( $cats != false )
-		 	$cats = $cats[0];
+		$rule_taxes = array_merge( $tags, $cats );
 
-	 	if( $tags != false )
-		 	$tags = $tags[0];
+		// Get product terms.
+		$product_terms = array_merge(
+			$product->get_category_ids(),
+			$product->get_tag_ids()
+		);
 
-		if ( $roles != false )
-		 	$roles = $roles[0];
+		// If the arrays intersect, apply rule based on priority.
+		if ( array_intersect( $rule_taxes, $product_terms ) ) {
+			$priority = get_post_meta( $rule->ID, '_priority', true );
 
-	 	$rule_taxes = array_merge( $tags, $cats );
-
-	 	// Loop through the Product's Categories
-	 	// If they are in the rule flag it
-	 	foreach ( $product_terms as $term ) {
-		 	if ( in_array( $term->term_id, $rule_taxes ) ) {
-			 	$apply_rule = true;
-		 	}
-	 	}
-
-	 	// If the rule applies, check the priority
-	 	if ( $apply_rule == true ) {
-
-	 		$priority = get_post_meta( $rule->ID, '_priority', true );
-
-	 		if( $priority != '' and $top > $priority or $top == null ) {
-	 			$top = $priority;
-	 			$top_rule = $rule;
-		 	}
+			if ( $priority !== '' && $top > $priority || $top === null ) {
+				$top      = $priority;
+				$top_rule = $rule;
+			}
 		}
 	}
 
